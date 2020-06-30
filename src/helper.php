461ac7addced41d1d8124822000fbe2da6b6718c
Andrey <alestro@bk.ru>
2020-06-30 22:33:34 +0200

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -1,14 +1,14 @@
 <?php
 
 
-namespace Lobster;
+namespace Bermuda\ErrorHandler;
 
 
 /**
  * @param \Throwable $e
  * @return int
  */
-function status_code(\Throwable $e) : int
+function get_status_code_from_throwable(\Throwable $e): int
 {
     return $e->getCode() >= 400 && $e->getCode() < 600 ? $e->getCode() : 500 ;
 }
