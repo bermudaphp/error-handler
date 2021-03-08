7f4fda70a865af0053203eafb6fc7eb5a429e80e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-03-08 16:56:22 +0300

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -2,15 +2,6 @@
 
 namespace Bermuda\ErrorHandler;
 
-use Whoops\RunInterface;
-use Psr\Container\ContainerInterface;
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Bermuda\Templater\RendererInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Bermuda\ErrorHandler\Generator\WhoopsErrorGenerator;
-use Bermuda\ErrorHandler\Generator\TemplateErrorGenerator;
-use Bermuda\RequestHandlerRunner\RequestHandlerRunnerFactory;
-
 final class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
     /**
