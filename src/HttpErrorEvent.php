a055b9a486090df3f86e219e2f10982f2d72733d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-28 12:51:45 +0300

    Create HttpErrorEvent.php

---
+++ src/HttpErrorEvent.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Bermuda\ErrorHandler;
+
+use Throwable;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Class HttpErrorEvent
+ * @package Bermuda\ErrorHandler
+ */
+final class HttpErrorEvent extends ErrorEvent
+{
+    private ResponseInterface $response;
+    private ServerRequestInterface $request;
+
+    public function __construct(Throwable $e, ServerRequestInterface $request, ResponseInterface $response)
+    {
+        parent::__construct($e);
+        $this->request = $request;
+        $this->response = $response;
+    }
+    
+    /**
+     * @param ResponseInterface|null $response
+     * @return ResponseInterface
+     */
+    public function response(?ResponseInterface $response = null): ResponseInterface
+    {
+        return $response != null ? $this->response = $response : $this->response;
+    }
+
+    /**
+     * @param ServerRequestInterface|null $request
+     * @return ServerRequestInterface|null
+     */
+    public function request(ServerRequestInterface $request = null): ServerRequestInterface
+    {
+        return $request != null ? $this->request = $request : $this->request;
+    }
+}
