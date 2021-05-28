ae183ae15996d8e8e86896f33f56b2bf38069afb
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-28 13:40:02 +0300

    Create ErrorHandlerTrait.php

---
+++ src/ErrorHandlerTrait.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Bermuda\ErrorHandler;
+
+use Throwable;
+use Bermuda\Eventor\EventDispatcher;
+use Psr\Http\Message\ResponseInterface;
+use Bermuda\Eventor\EventDispatcherInterface;
+use Bermuda\Eventor\Provider\PrioritizedProvider;
+
+/**
+ * Trait ErrorHandler
+ * @package Bermuda\ErrorHandler
+ */
+trait ErrorHandler
+{
+    private int $errorLevel;
+    private EventDispatcherInterface $dispatcher;
+    private ?PrioritizedProvider $provider = null;
+    private ErrorResponseGeneratorInterface $generator;
+   
+    public function __construct(ErrorResponseGeneratorInterface $generator,
+        EventDispatcherInterface $dispatcher = null, int $errorLevel = E_ALL
+    )
+    {
+        $this->setResponseGenerator($generator)
+            ->setDispatcher($dispatcher ?? new EventDispatcher())
+            ->errorLevel($errorLevel);
+    }
+    
+    public function setResponseGenerator(ErrorResponseGeneratorInterface $generator): self 
+    {
+        $this->generator = $generator;
+        return $this;
+    }
+    
+    /**
+     * Set error_reporting level or return current level if no level parameter is given. 
+     * @param int|null $level
+     * @return int
+     */
+    public function errorLevel(?int $level = null): int 
+    {
+        return $level != null ? $this->errorLevel = $level : $this->errorLevel;
+    }
+    
+    /**
+     * @param EventDispatcherInterface $dispatcher
+     * @return self
+     */
+    public function setDispatcher(EventDispatcherInterface $dispatcher): self
+    {
+        $this->provider ?: $this->provider = new PrioritizedProvider();
+        $this->dispatcher = $dispatcher->attach($this->provider);
+        
+        return $this;
+    }
+
+    /**
+     * @param ErrorListenerInterface $listener
+     * @return void
+     */
+    public function listen(ErrorListenerInterface $listener, int $priority = 0): void
+    {
+        $this->provider->listen($listener, $priority);
+    }
+    
+    private function generateResponse(RequestHandlingException $e): ResponseInterface
+    {
+        return $this->dispatcher(new HttpErrorEvent($e, $request, $this->generator->generate($e)))->response();
+    }
+}
