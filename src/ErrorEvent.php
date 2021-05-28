27814c4965869fdea2eabdbec970282839a88a79
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-28 12:52:34 +0300

    Update ErrorEvent.php

--- src/ErrorEvent.php
+++ src/ErrorEvent.php
@@ -3,24 +3,18 @@
 namespace Bermuda\ErrorHandler;
 
 use Throwable;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Class ErrorEvent
  * @package Bermuda\ErrorHandler
  */
-final class ErrorEvent
+class ErrorEvent
 {
     private Throwable $e;
-    private ResponseInterface $response;
-    private ServerRequestInterface $request;
-
-    public function __construct(Throwable $e, ServerRequestInterface $request, ResponseInterface $response)
+   
+    public function __construct(Throwable $e)
     {
         $this->e = $e;
-        $this->request = $request;
-        $this->response = $response;
     }
 
     /**
@@ -29,23 +23,5 @@ final class ErrorEvent
     public function getThrowable(): Throwable
     {
         return $this->e;
-    }
-
-    /**
-     * @param ResponseInterface|null $response
-     * @return ResponseInterface
-     */
-    public function response(?ResponseInterface $response = null): ResponseInterface
-    {
-        return $response != null ? $this->response = $response : $this->response;
-    }
-
-    /**
-     * @param ServerRequestInterface|null $request
-     * @return ServerRequestInterface|null
-     */
-    public function request(ServerRequestInterface $request = null): ServerRequestInterface
-    {
-        return $request != null ? $this->request = $request : $this->request;
     }
 }
