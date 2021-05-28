049be9c7a1aea2bd6eec8e87193f30aec765547c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-28 13:59:54 +0300

    Update and rename ServerRequestAwareInterface.php to RequestHandlingException.php

---
+++ src/RequestHandlingException.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Bermuda\ErrorHandler;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Class RequestHandlingException
+ * @package Bermuda\ErrorHandler
+ */
+final class RequestHandlingException extends RuntimeException
+{
+    private ServerRequestInterface $serverRequest;
+    
+    public function __construct(\Throwable $e, ServerRequestInterface $request)
+    {
+        $this->serverRequest = $request;
+        parent::__construct($e->getMessage(), $this->getStatusCode($e), $e);
+        $this->file = $e->getFile(); $this->line = $e->getLine();
+    }
+    
+    public function getServerRequest(): ServerRequestInterface
+    {
+        return $this->serverRequest;
+    }
+    
+    public static function getStatusCode(\Throwable $e): int
+    {
+        return $e->getCode() >= 400 && $e->getCode() < 600 ? $e->getCode() : 500 ;
+    }
+}
