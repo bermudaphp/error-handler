6b749313b23199704a6cdfc501c11d630952e4bd
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-05-29 15:20:44 +0300

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -2,6 +2,10 @@
 
 namespace Bermuda\ErrorHandler;
 
+/**
+ * Class ConfigProvider
+ * @package Bermuda\ErrorHandler
+ */
 final class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
     /**
@@ -9,6 +13,17 @@ final class ConfigProvider extends \Bermuda\Config\ConfigProvider
      */
     protected function getFactories(): array
     {
-        return [ErrorResponseGeneratorInterface::class => ErrorResponseGeneratorFactory::class];
+        return [
+            ErrorResponseGeneratorInterface::class => ErrorResponseGeneratorFactory::class,
+            ErrorHandlerInterface::class => ErrorHandlerFactory::class
+        ];
+    }
+    
+    /**
+     * @inheritDoc
+     */
+    protected function getAliases(): array
+    {
+        return [ErrorRendererInterface::class => ErrorHandlerInterface::class];
     }
 }
