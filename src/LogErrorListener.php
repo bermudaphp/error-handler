e0cc023aa4f72a1b009a1d5b5dc6f3624959c57b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-06-05 16:37:25 +0300

    Update LogErrorListener.php

--- src/LogErrorListener.php
+++ src/LogErrorListener.php
@@ -9,10 +9,11 @@ use Psr\Log\LoggerInterface;
  * Class LogErrorListener
  * @package Bermuda\ErrorHandler
  */
-class LogErrorListener implements ErrorListenerInterface
+final class LogErrorListener implements ErrorListenerInterface
 {
     private LoggerInterface $logger;
     private ErrorRendererInterface $renderer;
+    private array $except = [];
 
     public function __construct(LoggerInterface $logger, ErrorRendererInterface $renderer = null)
     {
@@ -20,11 +21,27 @@ class LogErrorListener implements ErrorListenerInterface
         $this->renderer = $renderer ?? WhoopsRenderer::plainTextRendering();
     }
 
+    public function except(string $errorType): self
+    {
+        $this->except[] = $errorType;
+        return $this;
+    }
+
     /**
      * @param ErrorEvent $event
      */
     public function __invoke(ErrorEvent $event): void
     {
-        $this->logger->error($this->renderer->renderException($event->getThrowable()));
+        if (!in_array($this->getExceptionClass($event), $this->except))
+        {
+            $this->logger->error($this->renderer->renderException($event->getThrowable()));
+        }
+    }
+
+    private function getExceptionClass(ErrorEvent $event): string
+    {
+        return get_class($event->getThrowable() instanceof HttpException ?
+            $event->getThrowable()->getPrevious() : $event->getThrowable()
+        );
     }
 }
