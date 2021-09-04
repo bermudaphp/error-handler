7ad221a4ffe2d837f91381b6b61d8257aca9371b
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-04 14:45:34 +0200

    Update ErrorHandlerFactory.php

--- src/ErrorHandlerFactory.php
+++ src/ErrorHandlerFactory.php
@@ -5,10 +5,6 @@ namespace Bermuda\ErrorHandler;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
 use Psr\Container\ContainerInterface;
 
-/**
- * Class ErrorHandlerFactory
- * @package Bermuda\ErrorHandler
- */
 final class ErrorHandlerFactory
 {
     public function __invoke(ContainerInterface $container): ErrorHandler
