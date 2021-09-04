02b1eb5429182af9863c7e71c05ec676b5315c35
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-04 14:31:19 +0200

    Update HttpException.php

--- src/HttpException.php
+++ src/HttpException.php
@@ -2,17 +2,15 @@
 
 namespace Bermuda\ErrorHandler;
 
+use Throwable;
+use RuntimeException;
 use Psr\Http\Message\ServerRequestInterface;
 
-/**
- * Class HttpException
- * @package Bermuda\ErrorHandler
- */
-final class HttpException extends \RuntimeException
+final class ServerRequestException extends RuntimeException
 {
     private ServerRequestInterface $serverRequest;
     
-    public function __construct(\Throwable $e, ServerRequestInterface $request)
+    public function __construct(Throwable $e, ServerRequestInterface $request)
     {
         $this->serverRequest = $request;
         parent::__construct($e->getMessage(), $this->getStatusCode($e), $e);
@@ -23,7 +21,7 @@ final class HttpException extends \RuntimeException
         return $this->serverRequest;
     }
     
-    public static function getStatusCode(\Throwable $e): int
+    public static function getStatusCode(Throwable $e): int
     {
         return $e->getCode() >= 400 && $e->getCode() < 600 ? $e->getCode() : 500 ;
     }
