f50985b639ae9b13ed70890a743f024e0ae3a15a
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-06 18:44:41 +0200

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -1,12 +1,18 @@
 <?php
 
 namespace Bermuda\ErrorHandler;
+use Throwable;
 
 /**
- * @param \Throwable $e
+ * @param Throwable $e
  * @return int
  */
-function get_status_code_from_throwable(\Throwable $e): int
+function get_status_code_from_throwable(Throwable $e): int
 {
-    return RequestHandlingException::getStatusCode($e);
+    return ServerException::getStatusCode($e);
+}
+
+function get_error_code(int $code): int
+{
+    return $code >= 400 && $code < 600 ? $code : 500 ;
 }
