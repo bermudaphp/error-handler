e4e930a43226e477a1462d97d804b3370f1e22a4
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-06 15:55:46 +0200

    Update ErrorResponseGeneratorInterface.php

--- src/ErrorResponseGeneratorInterface.php
+++ src/ErrorResponseGeneratorInterface.php
@@ -4,11 +4,7 @@ namespace Bermuda\ErrorHandler;
 
 use Psr\Http\Message\ResponseInterface;
 
-/**
- * Interface ErrorResponseGeneratorInterface
- * @package Bermuda\ErrorHandler
- */
 interface ErrorResponseGeneratorInterface
 {
-    public function generate(HttpException $e): ResponseInterface;
+    public function generate(ServerException $e): ResponseInterface;
 }
