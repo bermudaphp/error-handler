814161482d06d8b4296f2acd4081e1ad75c0a1e8
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-01-25 21:42:55 +0200

    Update ErrorHandlerFactory.php

--- src/ErrorHandlerFactory.php
+++ src/ErrorHandlerFactory.php
@@ -2,6 +2,7 @@
 
 namespace Bermuda\ErrorHandler;
 
+use Bermuda\Eventor\EventDispatcherInterface;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
 use Psr\Container\ContainerInterface;
 
@@ -10,8 +11,9 @@ final class ErrorHandlerFactory
     public function __invoke(ContainerInterface $container): ErrorHandler
     {
         return new ErrorHandler(
-            $container->get(ErrorResponseGeneratorInterface::class),
-            $container->get(EmitterInterface::class),
+            $container->get(ErrorResponseGeneratorInterface::class), $container->get(EmitterInterface::class),
+            null, null,
+            $container->get('config')['error']['errorLevel'] ?? E_ALL
         );
     }
 }
