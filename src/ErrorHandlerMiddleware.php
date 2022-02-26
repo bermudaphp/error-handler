ed1b6e4260ae627e71e8eef08c9a2d5721e4141f
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-26 19:28:57 +0200

    Update ErrorHandlerMiddleware.php

--- src/ErrorHandlerMiddleware.php
+++ src/ErrorHandlerMiddleware.php
@@ -30,37 +30,21 @@ final class ErrorHandlerMiddleware implements MiddlewareInterface
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $old = error_reporting($this->errorLevel);
-        set_error_handler($this->createHandler());
+        set_error_handler(static function(int $errno, string $msg, string $file, int $line): void {
+            if ((error_reporting() & $errno)) {
+                throw new \ErrorException($msg, 0, $errno, $file, $line);
+            }
+        });
 
-        try
-        {
+        try {
             $response = $handler->handle($request);
-        }
-
-        catch (Throwable $e)
-        {
-            $response = $this->generateResponse(new ServerException($e, $request));
+        } catch (Throwable $e) {
+            $response = $this->generator->generateResponse($e, $request);
         }
         
         restore_error_handler();
         error_reporting($old);
-
+        
         return $response;
-    }
-  
-    private function createHandler(): callable
-    {
-        return new class
-        {
-            public function __invoke(int $errno, string $msg, string $file, int $line)
-            {
-                if (!(error_reporting() & $errno))
-                {
-                    return;
-                }
-
-                throw new \ErrorException($msg, 0, $errno, $file, $line);
-            }
-        };
     }
 }
