76a543eb005ae61d544750a52036cc6bcc6f998c
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-26 16:27:49 +0200

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -9,10 +9,15 @@ use Throwable;
  */
 function get_status_code_from_throwable(Throwable $e): int
 {
-    return ServerException::getStatusCode($e);
+    return get_error_code($e->getCode());
 }
 
 function get_error_code(int $code): int
 {
     return $code >= 400 && $code < 600 ? $code : 500 ;
+}
+
+function createEvent(Throwable $e): ErrorEvent|ServerErrorEvent
+{
+    return $e instanceof ServerException ? new ServerErrorEvent($e) : new ErrorEvent($e);
 }
