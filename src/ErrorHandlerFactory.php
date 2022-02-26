6874c8d76389c23c07a6767017bf8fb17f545dd3
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-26 22:47:27 +0200

    Update ErrorHandlerFactory.php

--- src/ErrorHandlerFactory.php
+++ src/ErrorHandlerFactory.php
@@ -3,6 +3,7 @@
 namespace Bermuda\ErrorHandler;
 
 use Psr\Container\ContainerInterface;
+use Bermuda\Eventor\EventDispatche;
 use Bermuda\Eventor\EventDispatcherInterface;
 use Bermuda\ErrorHandler\Renderer\WhoopsRenderer;
 use Laminas\HttpHandlerRunner\Emitter\EmitterInterface;
@@ -14,7 +15,7 @@ final class ErrorHandlerFactory
         return new ErrorHandler(
             $container->get(ErrorResponseGeneratorInterface::class), $container->get(EmitterInterface::class),
             $container->has(ErrorRendererInterface::class) ? $container->get(ErrorRendererInterface::class) : new WhoopsRenderer,
-            $container->has(EventDispatcherInterface::class) ? $container->get(EventDispatcherInterface::class) : null,
+            $container->has(EventDispatcherInterface::class) ? $container->get(EventDispatcherInterface::class) : new EventDispatcher
         );
     }
 }
