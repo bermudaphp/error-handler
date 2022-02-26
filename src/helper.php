5c0b370b5006817982a2fc5d88d9f9afbcf82f66
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-26 20:21:48 +0200

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -21,3 +21,18 @@ function createEvent(Throwable $e): ErrorEvent|ServerErrorEvent
 {
     return $e instanceof ServerException ? new ServerErrorEvent($e->getPrevious(), $e->getServerRequest()) : new ErrorEvent($e);
 }
+
+function createListener(callable $callable, int $priority): ErrorListenerInterface
+{
+    return new class($callable, $priority = 1) implements ErrorListenerInterface {
+        use ErrorListener;
+        public function __construct(private $callable, int $priority)
+        {
+            $this->priority = $priority;
+        }
+        public function handleEvent(ErrorEvent $event): void 
+        {
+            ($this->callable)($event);
+        }
+    };
+}
