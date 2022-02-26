4d6917e6bd56ca5e553c1d4bc1906b2e936e92d7
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-26 13:57:35 +0200

    Update ErrorResponseGeneratorFactory.php

--- src/ErrorResponseGeneratorFactory.php
+++ src/ErrorResponseGeneratorFactory.php
@@ -13,18 +13,15 @@ final class ErrorResponseGeneratorFactory
 {
     public function __invoke(ContainerInterface $container): ErrorResponseGeneratorInterface
     {
-        if ($container->get('config')['debug'])
-        {
-            if ($container->has(RunInterface::class))
-            {
+        if ($container->get('config')['debug']) {
+            if ($container->has(RunInterface::class)) {
                 $run = $container->get(RunInterface::class);
             }
 
             return new WhoopsErrorGenerator($container->get(ResponseFactoryInterface::class), $run ?? null);
         }
         
-        return new TemplateErrorGenerator(static function ($code) use ($container)
-        {
+        return new TemplateErrorGenerator(static function ($code) use ($container) {
             return $container->get(RendererInterface::class)->render('errors::' . $code);
         }, $container->get(ResponseFactoryInterface::class));
     }
