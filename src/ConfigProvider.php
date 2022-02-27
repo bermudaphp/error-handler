20198e46e4b53db4851ee53cba44cca433990101
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-27 20:09:06 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -5,6 +5,9 @@ namespace Bermuda\ErrorHandler;
 use Psr\Container\ContainerInterface;
 use Generator\ErrorResponseGenerator;
 use Generator\WhoopsErrorGenerator;
+use Renderer\WhoopsRenderer;
+use Whoops\RunInterface;
+use Whoops\Run;
 use Psr\Http\Message\ResponseFactoryInterface;
 
 final class ConfigProvider extends \Bermuda\Config\ConfigProvider
@@ -17,7 +20,11 @@ final class ConfigProvider extends \Bermuda\Config\ConfigProvider
         return [
             ErrorHandler::class => ErrorHandlerFactory::class,
             ErrorResponseGenerator::class => fn(ContainerInterface $container) => new ErrorResponseGenerator($container->get(WhoopsErrorGenerator::class)),
-            WhoopsErrorGenerator::class => fn(ContainerInterface $container) => new WhoopsErrorGenerator($container->get(ResponseFactoryInterface::class))
+            WhoopsErrorGenerator::class => fn(ContainerInterface $container) => new WhoopsErrorGenerator($container->get(ResponseFactoryInterface::class)),
+            WhoopsRenderer::class => fn(ContainerInterface $container) => new WhoopsRenderer(
+                $container->has(RunInterface::class) ? $container->get(RunInterface::class) : new Run,
+                $container->get('config')['error']['error.renderer.configuator'] ?? null
+            ),
         ];
     }
     
