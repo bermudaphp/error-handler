86f5d436e2dcfbb6309df6896361cfe113c9b615
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-02-27 20:09:49 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -23,7 +23,7 @@ final class ConfigProvider extends \Bermuda\Config\ConfigProvider
             WhoopsErrorGenerator::class => fn(ContainerInterface $container) => new WhoopsErrorGenerator($container->get(ResponseFactoryInterface::class)),
             WhoopsRenderer::class => fn(ContainerInterface $container) => new WhoopsRenderer(
                 $container->has(RunInterface::class) ? $container->get(RunInterface::class) : new Run,
-                $container->get('config')['error']['error.renderer.configuator'] ?? null
+                $container->get('config')['error']['error.renderer.configurator'] ?? null
             ),
         ];
     }
