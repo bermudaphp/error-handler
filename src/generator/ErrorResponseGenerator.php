c9846d3b43b46c6245e9ada153c964f1c62ba99d
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-03 17:19:07 +0200

    Update ErrorResponseGenerator.php

--- src/generator/ErrorResponseGenerator.php
+++ src/generator/ErrorResponseGenerator.php
@@ -5,16 +5,39 @@ namespace Bermuda\ErrorHandler\Generator;
 use Throwable;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Bermuda\ErrorHandler\ErrorResponseGeneratorInterface;
+use Bermuda\HTTP\Contracts\ResponseFactoryAwareInterface;
 
-final class ErrorResponseGenerator implements ErrorResponseGeneratorInterface
+final class ErrorResponseGenerator implements ErrorResponseGeneratorInterface, ResponseFactoryAwareInterface
 {
     private array $generators = [];
-    public function __construct(private WhoopsErrorGenerator $whoopsErrorGenerator) {
+    public function __construct(private ResponseFactoryInterface $factory, private WhoopsErrorGenerator $whoopsErrorGenerator) {
+    }
+
+    /**
+     * @param ResponseFactoryInterface $factory
+     * @return ResponseFactoryAwareInterface
+     */
+    public function setResponseFactory(ResponseFactoryInterface $factory): ResponseFactoryAwareInterface
+    {
+        $this->factory = $factory;
+        
+        foreach ($this->generators as $generator) {
+            if ($generator instanceof ResponseFactoryAwareInterface) {
+                $generator->setResponseFactory($factory);
+            }
+        }
+        
+        return $this;
     }
 
     public function addGenerator(ErrorResponseGeneratorInterface $generator): self
     {
+        if ($generator instanceof ResponseFactoryAwareInterface) {
+            $generator->setResponseFactory($this->responseFactory);
+        }
+        
         array_unshift($this->generators, $generator);
         return $this;
     }
