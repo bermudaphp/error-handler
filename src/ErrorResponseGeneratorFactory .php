e973a4a918b706d10e6ff4a8d5018144a022dfce
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-03 17:14:41 +0200

    Update ErrorResponseGeneratorFactory .php

--- src/ErrorResponseGeneratorFactory .php
+++ src/ErrorResponseGeneratorFactory .php
@@ -5,11 +5,12 @@ namespace Bermuda\ErrorHandler;
 use Bermuda\ErrorHandler\Generator\ErrorResponseGenerator;
 use Bermuda\ErrorHandler\Generator\WhoopsErrorGenerator;
 use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 
 final class ErrorResponseGeneratorFactory 
 {
     public function __invoke(ContainerInterface $container): ErrorResponseGenerator
     {
-        return new ErrorResponseGenerator($container->get(WhoopsErrorGenerator::class));
+        return new ErrorResponseGenerator($container->get(ResponseFactoryInterface::class), $container->get(WhoopsErrorGenerator::class));
     }
 }
