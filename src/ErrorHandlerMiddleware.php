4a2fb47bcf415efd7f031e4043d935d1a0b7985e
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-08 20:58:06 +0200

    Update ErrorHandlerMiddleware.php

--- src/ErrorHandlerMiddleware.php
+++ src/ErrorHandlerMiddleware.php
@@ -9,18 +9,24 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use Bermuda\Eventor\EventDispatcherInterface;
-use Bermuda\Eventor\Provider\PrioritizedProvider;
+use Bermuda\Eventor\EventDispatcherAwareInterface;
 
 final class ErrorHandlerMiddleware implements MiddlewareInterface, EventDispatcherAwareInterface
 {
-    private PrioritizedProvider $provider;
-    public function __construct(private Generator\ErrorResponseGenerator $generator, 
-        private EventDispatcherInterface $dispatcher = new ErrorDispatcher, private int $errorLevel = E_ALL
+    public function __construct(private ErrorHandler $errorHandler, private int $errorLevel = E_ALL
     ) {
-        $this->provider = new PrioritizedProvider;
-        $this->dispatcher = $dispatcher->attach($this->provider);
     }
-    
+
+    /**
+     * @param EventDispatcherInterface $dispatcher
+     * @return self
+     */
+    public function setDispatcher(EventDispatcherInterface $dispatcher): EventDispatcherAwareInterface
+    {
+        $this->errorHandler->setDispatcher($dispatcher);
+        return $this;
+    }
+
     /**
      * @inheritDoc
      */
@@ -36,8 +42,7 @@ final class ErrorHandlerMiddleware implements MiddlewareInterface, EventDispatch
         try {
             $response = $handler->handle($request);
         } catch (Throwable $e) {
-            $response = $this->generator->generateResponse($e, $request);
-            $this->dispatcher->dispatch(new ServerErrorEvent($e, $request));
+            $response = $this->errorHandler->generateResponse($e, $request);
         }
         
         restore_error_handler();
@@ -47,26 +52,12 @@ final class ErrorHandlerMiddleware implements MiddlewareInterface, EventDispatch
     }
     
     /**
-     * @param EventDispatcherInterface $dispatcher
-     * @return self
-     */
-    public function setDispatcher(EventDispatcherInterface $dispatcher): EventDispatcherAwareInterface
-    {
-        if ($this->provider == null) {
-            $this->provider = new PrioritizedProvider;
-        }
-
-        $this->dispatcher = $dispatcher->attach($this->provider);
-        return $this;
-    }
-
-    /**
      * @param ErrorListenerInterface $listener
      * @return static
      */
     public function on(ErrorListenerInterface $listener): self
     {
-        $this->provider->listen(ServerErrorEvent::class, $listener, $listener->getPriority());
+        $this->errorHandler->on($listener);
         return $this;
     }
 }
