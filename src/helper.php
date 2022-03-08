ff8ab7c3c73dc40bdb264e9c2b5ff9cd0e695875
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-08 23:48:34 +0200

    Update helper.php

--- src/helper.php
+++ src/helper.php
@@ -22,18 +22,16 @@ function get_error_code(int|Throwable $code): int
     return $code >= 400 && $code < 600 ? $code : 500 ;
 }
 
-function createEvent(Throwable $e, ?ServerRequestInterface $serverRequest = null): ErrorEvent|ServerErrorEvent
+function createEvent(Throwable $e): ErrorEvent|ServerErrorEvent
 {
     if ($e instanceof ServerException) {
-        return new ServerErrorEvent($e->getPrevious(), $e->getServerRequest());
-    } elseif ($serverRequest != null) {
-        return new ServerErrorEvent($e, $serverRequest);
+        return new ServerErrorEvent($e->throwable, $e->serverRequest);
     }
     
     return new ErrorEvent($e);
 }
 
-function createListener(callable $callable, int $priority): ErrorListenerInterface
+function createListener(callable $callable, int $priority = 1): ErrorListenerInterface
 {
     return new class($callable, $priority = 1) implements ErrorListenerInterface {
         use ErrorListener;
