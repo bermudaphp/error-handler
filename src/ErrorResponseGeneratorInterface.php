37a52a92fa094ddb0613b7d46511c6fa609f4579
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-08 21:34:49 +0200

    Update ErrorResponseGeneratorInterface.php

--- src/ErrorResponseGeneratorInterface.php
+++ src/ErrorResponseGeneratorInterface.php
@@ -10,18 +10,16 @@ interface ErrorResponseGeneratorInterface
 {
     /**
      * @param Throwable $e
-     * @param ServerRequestInterface|null $request
      * @return bool
      * returns true if the generator can handle the exception, otherwise returns false
      */
-    public function canGenerate(Throwable $e, ServerRequestInterface $request = null): bool ;
+    public function canGenerate(Throwable $e): bool ;
 
     /**
      * @param Throwable $e
-     * @param ServerRequestInterface|null $request
      * @return ResponseInterface
      * @throws Throwable if self::canGenerate method return false
      */
-    public function generateResponse(Throwable $e, ServerRequestInterface $request = null): ResponseInterface ;
+    public function generateResponse(Throwable $e): ResponseInterface ;
 }
 
