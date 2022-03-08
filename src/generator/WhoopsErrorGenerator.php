e92999f29fa6aa416d66b4e46a73a3ce7afc79bc
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2022-03-08 21:47:41 +0200

    Update WhoopsErrorGenerator.php

--- src/generator/WhoopsErrorGenerator.php
+++ src/generator/WhoopsErrorGenerator.php
@@ -12,12 +12,14 @@ use Bermuda\ErrorHandler\ServerException;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Bermuda\ErrorHandler\Renderer\WhoopsRenderer;
+use Bermuda\HTTP\Contracts\ServerRequestAwareTrait;
 use Bermuda\HTTP\Contracts\ServerRequestAwareInterface;
 use Bermuda\ErrorHandler\ErrorResponseGeneratorInterface;
 use function Bermuda\ErrorHandler\get_error_code;
 
-final class WhoopsErrorGenerator implements ErrorResponseGeneratorInterface
+final class WhoopsErrorGenerator implements ErrorResponseGeneratorInterface, ServerRequestAwareInterface
 {
+    use ServerRequestAwareTrait;
     public function __construct(private ResponseFactoryInterface $responseFactory, private WhoopsRenderer $renderer = new WhoopsRenderer) {
     }
 
@@ -33,7 +35,7 @@ final class WhoopsErrorGenerator implements ErrorResponseGeneratorInterface
     {
         $renderer = $this->renderer;
         
-        if (($request = $e?->serverRequest) != null && $renderer instanceof ServerRequestAwareInterface) {
+        if (($request = $e?->serverRequest ?? $this->serverRequest) != null && $renderer instanceof ServerRequestAwareInterface) {
             ($renderer = clone $renderer)->setServerRequest($request);
         }
         
