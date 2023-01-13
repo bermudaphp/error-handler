d528df950124ef465eb161908377912b1eec4334
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-01-13 17:11:43 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -4,6 +4,7 @@ namespace Bermuda\ErrorHandler;
 
 use Whoops\Run;
 use Whoops\RunInterface;
+use Bermuda\Config\Config;
 use Psr\Container\ContainerInterface;
 use Bermuda\ErrorHandler\Generator\ErrorResponseGenerator;
 use Psr\Http\Message\ResponseFactoryInterface;
@@ -12,6 +13,10 @@ use Bermuda\ErrorHandler\Generator\WhoopsErrorGenerator;
 
 final class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
+    const error_handling = 'error_handling';
+    const error_level = 'error_level';
+    const error_whoops_renderer_configurator = 'error_whoops_renderer_configurator';
+
     /**
      * @inheritDoc
      */
@@ -21,14 +26,14 @@ final class ConfigProvider extends \Bermuda\Config\ConfigProvider
             ErrorHandler::class => ErrorHandlerFactory::class,
             ErrorHandlerMiddleware::class => static function(ContainerInterface $container) {
                 return new ErrorHandlerMiddleware($container->get(ErrorHandler::class), 
-                    $container->get('config')['error']['errorLevel'] ?? E_ALL
+                    $container->get(Config::app_config)[self::error_handling][self::error_level] ?? E_ALL
                 );
             },
             ErrorResponseGenerator::class => ErrorResponseGeneratorFactory::class,
             WhoopsErrorGenerator::class => static fn(ContainerInterface $container) => new WhoopsErrorGenerator($container->get(ResponseFactoryInterface::class)),
             WhoopsRenderer::class => static fn(ContainerInterface $container) => new WhoopsRenderer(
                 $container->has(RunInterface::class) ? $container->get(RunInterface::class) : new Run,
-                $container->get('config')['error']['error.renderer.configurator'] ?? null
+                $container->get(Config::app_config)[self::error_handling][self::error_whoops_renderer_configurator] ?? null
             ),
         ];
     }
